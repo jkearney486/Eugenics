458637d61c75dfc4a34a50c1dec32d41ee2a5cb3
jkearney486 <jkearney486@gmail.com>
2015-12-29 13:50:58 -0500

    strip out bootstrap, add knockout

--- Eugenics/Eugenics/Scripts/_references.js
+++ Eugenics/Eugenics/Scripts/_references.js
@@ -1,3 +1,5 @@
 ﻿/// <reference path="jquery-2.1.4.intellisense.js" />
-/// <reference path="bootstrap.js" />
 /// <reference path="modernizr-2.8.3.js" />
+/// <reference path="knockout-3.4.0.debug.js" />
+/// <reference path="knockout-delegatedEvents.js" />
+/// <reference path="knockout-postbox.js" />
