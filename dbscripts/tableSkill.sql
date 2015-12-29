5abc5c5a197454851d9cc7c277bbbf75ba860b17
jkearney486 <jkearney486@gmail.com>
2015-12-29 14:38:31 -0500

    add Skill table and data

---
+++ dbscripts/tableSkill.sql
@@ -0,0 +1,31 @@
+USE [Eugenics]
+GO
+
+/****** Object:  Table [dbo].[Skill]    Script Date: 12/29/2015 2:37:00 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Skill](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+	[Description] [varchar](500) NOT NULL,
+	[ActivationStat] [varchar](50) NULL,
+	[ActivationMultiplier] [float] NULL,
+ CONSTRAINT [PK_Skill] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
