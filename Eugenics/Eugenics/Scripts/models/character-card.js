24393a388a480fed8e84ea8db0ea55fb15f2cc6a
jkearney486 <jkearney486@gmail.com>
2016-01-08 21:37:10 -0500

    hide Parents tab for non-children, dynamically assign ids/hrefs for tab sets

--- Eugenics/Eugenics/Scripts/models/character-card.js
+++ Eugenics/Eugenics/Scripts/models/character-card.js
@@ -6,10 +6,57 @@
         "text!../views/character-card.html"
     ], function (ko, htmlString) {
         var CharacterCardViewModel = function (params) {
+            this.cardIndex = params.index;
             this.character = params.character;
             this.characters = params.characters;
             this.classes = params.classes;
             this.skills = params.skills;
+
+            this.parentsTabId = ko.computed({
+                read: function () {
+                    var href = "parents-tab";
+                    href += this.cardIndex();
+                    return href;
+                },
+                owner: this
+            });
+            this.classesTabId = ko.computed({
+                read: function () {
+                    var href = "classes-tab";
+                    href += this.cardIndex();
+                    return href;
+                },
+                owner: this
+            });
+            this.skillsTabId = ko.computed({
+                read: function () {
+                    var href = "skills-tab";
+                    href += this.cardIndex();
+                    return href;
+                },
+                owner: this
+            });
+            this.parentsTabHref = ko.computed({
+                read: function () {
+                    return "#" + this.parentsTabId();
+                },
+                deferEvaluation: true,
+                owner: this
+            });
+            this.classesTabHref = ko.computed({
+                read: function () {
+                    return "#" + this.classesTabId();
+                },
+                deferEvaluation: true,
+                owner: this
+            });
+            this.skillsTabHref = ko.computed({
+                read: function () {
+                    return "#" + this.skillsTabId();
+                },
+                deferEvaluation: true,
+                owner: this
+            });
         };
 
 
